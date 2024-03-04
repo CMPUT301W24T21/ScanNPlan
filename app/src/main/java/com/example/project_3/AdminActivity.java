b67b21daa5364902e9923e781073e531d7254a49
Akhil Pullela <akpullela2@gmail.com>
2024-03-04 16:24:06 -0700

    Created activity for admin homepage

---
+++ app/src/main/java/com/example/project_3/AdminActivity.java
@@ -0,0 +1,29 @@
+package com.example.project_3;
+
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+
+public class AdminActivity extends AppCompatActivity {
+    private MaterialButton manage_profiles;
+    private MaterialButton manage_images;
+    private MaterialButton manage_events;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_homepage);
+        this.manage_events = findViewById(R.id.manage_events);
+        this.manage_images = findViewById(R.id.manage_images);
+        this.manage_profiles = findViewById(R.id.manage_profiles);
+        manage_events.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
