270f0c0f5f96088b7e415d77b818485602fc8c74
Akhil Pullela <akpullela2@gmail.com>
2024-03-05 14:55:36 -0700

    Added new Fragment for Browse Profiles

---
+++ app/src/main/java/com/example/project_3/BrowseProfilesFragment.java
@@ -0,0 +1,26 @@
+package com.example.project_3;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class BrowseProfilesFragment extends Fragment {
+
+    public BrowseProfilesFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.admin_profiles, container, false);
+    }
+}
