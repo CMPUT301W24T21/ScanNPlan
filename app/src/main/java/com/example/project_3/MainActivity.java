57880bbe7b3e9ed88a8b9ddd8d725bfedc3de5c8
Akhil Pullela <akpullela2@gmail.com>
2024-03-06 00:05:12 -0700

    Fixed MainActivity crashing

--- app/src/main/java/com/example/project_3/MainActivity.java
+++ app/src/main/java/com/example/project_3/MainActivity.java
@@ -6,14 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-     Intent AttendeeIntent = new Intent(this, AttendeeActivity.class);
+    private Intent AttendeeIntent;
+    private Intent AdminIntent;
     //Intent AdminIntent = new Intent(this, );
-    //Intent OrganizerIntent = new Intent(this, );
+    private Intent OrganizerIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AttendeeIntent = new Intent(this, AttendeeActivity.class);
+        AdminIntent = new Intent(this, AdminActivity.class);
+        //OrganizerIntent = new Intent(this, );
+
 
         //This line starts the attendee activity
         startActivity(AttendeeIntent);
