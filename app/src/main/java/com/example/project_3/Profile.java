462fec12c4a3b9e88fa99d921a40069061938865
AidenKly <aklymchu@ualberta.ca>
2024-03-07 20:44:14 -0700

    Changed user and profile class to be more helpful

--- app/src/main/java/com/example/project_3/Profile.java
+++ app/src/main/java/com/example/project_3/Profile.java
@@ -1,13 +1,16 @@
 package com.example.project_3;
 
 import android.graphics.drawable.Icon;
-import android.media.Image;
+
+import java.util.List;
 
 public class Profile {
+    private String ProfileID;
     private Icon profile_picture;
     private String name;
     private String contact_info;
     private String social_link;
+    private List<Event> events;
 
     public Profile(Icon image, String name, String contact_info, String social_link){
         this.profile_picture = image;
@@ -16,34 +19,38 @@ public class Profile {
         this.social_link = social_link;
     }
 
-    public void setProfilePicture(Icon profile_picture) {
+    public Icon getProfile_picture() {
+        return profile_picture;
+    }
+
+    public void setProfile_picture(Icon profile_picture) {
         this.profile_picture = profile_picture;
     }
 
-    public void setContactInfo(String contact_info) {
-        this.contact_info = contact_info;
+    public String getName() {
+        return name;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setSocialLink(String social_link) {
+    public String getContact_info() {
+        return contact_info;
+    }
+
+    public void setContact_info(String contact_info) {
+        this.contact_info = contact_info;
+    }
+
+    public String getSocial_link() {
+        return social_link;
+    }
+
+    public void setSocial_link(String social_link) {
         this.social_link = social_link;
     }
-
-    public Icon getProfilePicture(){
-        return this.profile_picture;
-    }
-    public String getName(){
-        return this.name;
-    }
-    public String getContactInfo(){
-        return this.contact_info;
-    }
-    public String getSocialLink(){
-        return this.social_link;
+    public void addEvent(Event event) {
 
     }
-
 }
