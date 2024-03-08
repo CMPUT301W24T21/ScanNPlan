be67174b943cb8f215907edf301b3208b7a048c8
Munchy <malchi@ualberta.ca>
2024-03-08 12:10:22 -0700

    comments for grid images

--- app/src/main/java/com/example/project_3/GridImagesArrayAdapter.java
+++ app/src/main/java/com/example/project_3/GridImagesArrayAdapter.java
@@ -9,6 +9,9 @@ import androidx.annotation.NonNull;
 
 import java.util.ArrayList;
 
+/**
+ * !Once we are able to use images able to build out this file to show images
+ */
 public class GridImagesArrayAdapter extends ArrayAdapter {
     // images will be dealt with after halfway checkpoint
     public GridImagesArrayAdapter(@NonNull Context context, ArrayList<Profile> profiles){
