1d0f737bf293d9056889b594f547623ea74923c2
Kirill Kotlyarchuk <kotlyarc@ualberta.ca>
2024-03-19 14:43:45 -0600

    just added some functionality to view a base64 string in EventDetailsActivity

--- app/src/main/java/com/example/project_3/Event.java
+++ app/src/main/java/com/example/project_3/Event.java
@@ -1,12 +1,235 @@
 package com.example.project_3;
+//
+//import android.graphics.Bitmap;
+//import android.graphics.BitmapFactory;
+//import android.util.Base64;
+//
+///**
+// * Represents an event with various details.
+// */
+//public class Event {
+//    private String name;
+//    private Boolean promo;
+//    private Boolean reuse;
+//    private byte[] qrCode; // Add QR code byte array field
+//    private String time;
+//    private String date;
+//    private String location;
+//    private String details;
+//    private String image;
+//    private Bitmap poster_picture;
+//
+//    /**
+//     * Constructs an event with specified details.
+//     *
+//     * @param name     The name of the event.
+//     * @param date     The date of the event.
+//     * @param time     The time of the event.
+//     * @param location The location of the event.
+//     * @param details  Details of the event.
+//     * @param promo    Whether the event is promotional.
+//     * @param reuse    Whether the event can be reused.
+//     * @param image    The image associated with the event.
+//     */
+//    public Event(String name, String date, String time, String location,
+//                 String details, boolean promo, boolean reuse, String image) {
+//        this.name = name;
+//        this.promo = promo;
+//        this.reuse = reuse;
+//        this.date = date;
+//        this.time = time;
+//        this.location = location;
+//        this.details = details;
+//        this.image = image;
+//    }
+//
+//    /**
+//     * Constructs an event with specified name and date.
+//     *
+//     * @param name The name of the event.
+//     * @param date The date of the event.
+//     */
+//    public Event(String name, String date) {
+//        this.name = name;
+//        this.date = date;
+//    }
+//
+//    /**
+//     * Constructs an event with specified details.
+//     *
+//     * @param name     The name of the event.
+//     * @param date     The date of the event.
+//     * @param time     The time of the event.
+//     * @param location The location of the event.
+//     * @param details  Details of the event.
+//     */
+//    public Event(String name, String date, String time,
+//                String location, String details) {
+//            this.name = name;
+//            this.date = date;
+//            this.time = time;
+//            this.location = location;
+//            this.details = details;
+//        }
+//
+//    /**
+//     * Retrieves the image associated with the event.
+//     *
+//     * @return The image associated with the event.
+//     */
+//    public String getImage() {
+//        return image;
+//    }
+//
+//
+//    /**
+//     * Sets the image associated with the event.
+//     *
+//     * @param image The image to be associated with the event.
+//     */
+//    public void setImage(String image) {
+//        this.image = image;
+//    }
+//
+//
+//    /**
+//     * Retrieves the time of the event.
+//     *
+//     * @return The time of the event.
+//     */
+//    public String getTime() {
+//        return this.time;
+//    }
+//
+//
+//    /**
+//     * Sets the time of the event.
+//     *
+//     * @param time The time to be set for the event.
+//     */
+//    public void setTime(String time) {
+//        this.time = time;
+//    }
+//
+//    /**
+//     * Retrieves the date of the event.
+//     *
+//     * @return The date of the event.
+//     */
+//    public String getDate() {
+//        return this.date;
+//    }
+//
+//
+//    /**
+//     * Sets the date of the event.
+//     *
+//     * @param date The date to be set for the event.
+//     */
+//    public void setDate(String date) {
+//        this.date = date;
+//    }
+//
+//    /**
+//     * Retrieves the location of the event.
+//     *
+//     * @return The location of the event.
+//     */
+//    public String getLocation() {return location;}
+//
+//    /**
+//     * Sets the location of the event.
+//     *
+//     * @param location The location to be set for the event.
+//     */
+//    public void setLocation(String location) {this.location = location;}
+//
+//    /**
+//     * Retrieves the details of the event.
+//     *
+//     * @return The details of the event.
+//     */
+//    public String getDetails() {return details;}
+//
+//    /**
+//     * Sets the details of the event.
+//     *
+//     * @param details The details to be set for the event.
+//     */
+//    public void setDetails(String details) {this.details = details;}
+//
+//    /**
+//     * Retrieves the name of the event.
+//     *
+//     * @return The name of the event.
+//     */
+//    public String getName() {
+//        return name;
+//    }
+//
+//    /**
+//     * Sets the name of the event.
+//     *
+//     * @param name The name to be set for the event.
+//     */
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    /**
+//     * Retrieves whether the event is promotional.
+//     *
+//     * @return True if the event is promotional, false otherwise.
+//     */
+//    public Boolean getPromo() {
+//        return this.promo;
+//    }
+//
+//    /**
+//     * Sets whether the event is promotional.
+//     *
+//     * @param promo Whether the event is promotional.
+//     */
+//    public void setPromo(Boolean promo) {
+//        this.promo = promo;
+//    }
+//
+//    /**
+//     * Retrieves whether the event can be reused.
+//     *
+//     * @return True if the event can be reused, false otherwise.
+//     */
+//    public Boolean getReuse() {
+//        return this.reuse;
+//    }
+//
+//    /**
+//     * Sets whether the event can be reused.
+//     *
+//     * @param reuse Whether the event can be reused.
+//     */
+//    public void setReuse(Boolean reuse) {
+//        this.reuse = reuse;
+//    }
+//
+//    /**
+//     * Retrieves the QR code associated with the event.
+//     *
+//     * @return The QR code associated with the event.
+//     */
+//    public byte[] getQrCode() {return this.qrCode;}
+//
+//    /**
+//     * Sets the QR code associated with the event.
+//     *
+//     * @param qrCode The QR code to be associated with the event.
+//     */
+//    public void setQrCode(byte[] qrCode) {this.qrCode = qrCode;}
+//}
 
-import android.media.Image;
-
-import com.google.type.DateTime;
-
-import java.sql.Time;
-import java.time.LocalDateTime;
-import java.util.Date;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
 
 /**
  * Represents an event with various details.
@@ -21,6 +244,7 @@ public class Event {
     private String location;
     private String details;
     private String image;
+    private Bitmap poster_picture;
 
     /**
      * Constructs an event with specified details.
@@ -43,7 +267,14 @@ public class Event {
         this.time = time;
         this.location = location;
         this.details = details;
-        this.image = image;
+
+        if (image != null && !image.isEmpty()) {
+            byte[] decodedBytes = Base64.decode(image, Base64.DEFAULT);
+            this.poster_picture = BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
+        }
+        else{
+            this.poster_picture = null;
+        }
     }
 
     /**
@@ -67,13 +298,13 @@ public class Event {
      * @param details  Details of the event.
      */
     public Event(String name, String date, String time,
-                String location, String details) {
-            this.name = name;
-            this.date = date;
-            this.time = time;
-            this.location = location;
-            this.details = details;
-        }
+                 String location, String details) {
+        this.name = name;
+        this.date = date;
+        this.time = time;
+        this.location = location;
+        this.details = details;
+    }
 
     /**
      * Retrieves the image associated with the event.
--- app/src/main/java/com/example/project_3/MainActivity.java
+++ app/src/main/java/com/example/project_3/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -40,102 +41,102 @@ public class MainActivity extends AppCompatActivity {
         OrganizerIntent = new Intent(this, OrganizerActivity.class);
         AdminIntent = new Intent(this, AdminActivity.class);
 
-//        setContentView(R.layout.account_selector);
-//        Button Attendee_button = findViewById(R.id.Attendee_button);
-//        Button Organizer_button = findViewById(R.id.Organizer_button);
-//        Button Admin_button = findViewById(R.id.Admin_button);
-//
-//        Attendee_button.setOnClickListener(V -> {
-//            AttendeeIntent = new Intent(this, AttendeeActivity.class);
-//            startActivity(AttendeeIntent);
-//        });
-//        Organizer_button.setOnClickListener(V -> {
-//            OrganizerIntent = new Intent(this, OrganizerActivity.class);
-//            startActivity(OrganizerIntent);
-//        });
-//        Admin_button.setOnClickListener(V -> {
-//            AdminIntent = new Intent(this, AdminActivity.class);
-//            startActivity(AdminIntent);
-//        });
+        setContentView(R.layout.account_selector);
+        Button Attendee_button = findViewById(R.id.Attendee_button);
+        Button Organizer_button = findViewById(R.id.Organizer_button);
+        Button Admin_button = findViewById(R.id.Admin_button);
 
-
-
-        //profileId = "This is an admin profile";
-        profileId = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
-        Log.d("DEBUG", profileId);
-
-        db = FirebaseFirestore.getInstance();
-        profilesRef = db.collection("Profiles");
-        Log.d("DEBUG", "about to connect to DB");
-
-        profilesRef.addSnapshotListener(new EventListener<QuerySnapshot>() {
-            @Override
-            public void onEvent(@Nullable QuerySnapshot querySnapshots,
-                                @Nullable FirebaseFirestoreException error) {
-                if (error != null) {
-                    Log.e("Firestore", error.toString());
-                    return;
-                }
-                if (querySnapshots != null) {
-                    boolean idFound = false;
-                    for (QueryDocumentSnapshot doc : querySnapshots) {
-                        String profileDocId = doc.getId();
-                        if (profileDocId.equals(profileId)) {
-                            idFound = true;
-                            profileDocDetails = doc.getData();
-                            user = new User(new Profile(null,
-                                    (String) profileDocDetails.get("name"),
-                                    (String) profileDocDetails.get("contact_info"),
-                                    (String) profileDocDetails.get("social_link"),
-                                    (String) profileDocDetails.get("profile_type")));
-                            user.getUserProfile().setProfileID(doc.getId());
-                            //get the id and from here start the valid intent lol?
-                            //please work????
-                            Log.d("DEBUG", user.getUserProfile().getProfileType());
-                            if (Objects.equals(user.getUserProfile().getProfileType(), "Attendee")){
-                                startActivity(AttendeeIntent);
-                            } else if (Objects.equals(user.getUserProfile().getProfileType(), "Organizer")) {
-                                startActivity(OrganizerIntent);
-                            } else if (Objects.equals(user.getUserProfile().getProfileType(), "Admin")) {
-                                startActivity(AdminIntent);
-                            }
-
-                            // Start the AttendeeIntent here
-
-//                            AttendeeIntent = new Intent(MainActivity.this, AttendeeActivity.class);
-//                            startActivity(AttendeeIntent);
-                        }
-                    }
-                    if (!idFound){
-                        user = new User(new Profile(null,
-                                                "New User!",
-                                                "",
-                                                "",
-                                                "Attendee"));
-                        user.getUserProfile().setProfileID(profileId);
-
-                        HashMap<String, Object> profileData = new HashMap<>();
-                        profileData.put("profile_picture", user.getUserProfile().getProfile_picture());
-                        profileData.put("name", user.getUserProfile().getName());
-                        profileData.put("contact_info", user.getUserProfile().getContact_info());
-                        profileData.put("social_link", user.getUserProfile().getSocial_link());
-                        profileData.put("profile_type", user.getUserProfile().getProfileType());
-                        profilesRef.document(profileId).set(profileData)
-                                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                    @Override
-                                    public void onSuccess(Void aVoid) {
-                                        Log.d("Firestore", "Event added with checkbox selections");
-                                    }
-                                })
-                                .addOnFailureListener(new OnFailureListener() {
-                                    @Override
-                                    public void onFailure(@NonNull Exception e) {
-                                        Log.e("Firestore", "Error adding event with checkbox selections", e);
-                                    }
-                                });
-                    }
-                }
-            }
+        Attendee_button.setOnClickListener(V -> {
+            AttendeeIntent = new Intent(this, AttendeeActivity.class);
+            startActivity(AttendeeIntent);
         });
+        Organizer_button.setOnClickListener(V -> {
+            OrganizerIntent = new Intent(this, OrganizerActivity.class);
+            startActivity(OrganizerIntent);
+        });
+        Admin_button.setOnClickListener(V -> {
+            AdminIntent = new Intent(this, AdminActivity.class);
+            startActivity(AdminIntent);
+        });
+
+
+//
+//        //profileId = "This is an admin profile";
+//        profileId = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
+//        Log.d("DEBUG", profileId);
+//
+//        db = FirebaseFirestore.getInstance();
+//        profilesRef = db.collection("Profiles");
+//        Log.d("DEBUG", "about to connect to DB");
+//
+//        profilesRef.addSnapshotListener(new EventListener<QuerySnapshot>() {
+//            @Override
+//            public void onEvent(@Nullable QuerySnapshot querySnapshots,
+//                                @Nullable FirebaseFirestoreException error) {
+//                if (error != null) {
+//                    Log.e("Firestore", error.toString());
+//                    return;
+//                }
+//                if (querySnapshots != null) {
+//                    boolean idFound = false;
+//                    for (QueryDocumentSnapshot doc : querySnapshots) {
+//                        String profileDocId = doc.getId();
+//                        if (profileDocId.equals(profileId)) {
+//                            idFound = true;
+//                            profileDocDetails = doc.getData();
+//                            user = new User(new Profile(null,
+//                                    (String) profileDocDetails.get("name"),
+//                                    (String) profileDocDetails.get("contact_info"),
+//                                    (String) profileDocDetails.get("social_link"),
+//                                    (String) profileDocDetails.get("profile_type")));
+//                            user.getUserProfile().setProfileID(doc.getId());
+//                            //get the id and from here start the valid intent lol?
+//                            //please work????
+//                            Log.d("DEBUG", user.getUserProfile().getProfileType());
+//                            if (Objects.equals(user.getUserProfile().getProfileType(), "Attendee")){
+//                                startActivity(AttendeeIntent);
+//                            } else if (Objects.equals(user.getUserProfile().getProfileType(), "Organizer")) {
+//                                startActivity(OrganizerIntent);
+//                            } else if (Objects.equals(user.getUserProfile().getProfileType(), "Admin")) {
+//                                startActivity(AdminIntent);
+//                            }
+//
+//                            // Start the AttendeeIntent here
+//
+////                            AttendeeIntent = new Intent(MainActivity.this, AttendeeActivity.class);
+////                            startActivity(AttendeeIntent);
+//                        }
+//                    }
+//                    if (!idFound){
+//                        user = new User(new Profile(null,
+//                                                "New User!",
+//                                                "",
+//                                                "",
+//                                                "Attendee"));
+//                        user.getUserProfile().setProfileID(profileId);
+//
+//                        HashMap<String, Object> profileData = new HashMap<>();
+//                        profileData.put("profile_picture", user.getUserProfile().getProfile_picture());
+//                        profileData.put("name", user.getUserProfile().getName());
+//                        profileData.put("contact_info", user.getUserProfile().getContact_info());
+//                        profileData.put("social_link", user.getUserProfile().getSocial_link());
+//                        profileData.put("profile_type", user.getUserProfile().getProfileType());
+//                        profilesRef.document(profileId).set(profileData)
+//                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+//                                    @Override
+//                                    public void onSuccess(Void aVoid) {
+//                                        Log.d("Firestore", "Event added with checkbox selections");
+//                                    }
+//                                })
+//                                .addOnFailureListener(new OnFailureListener() {
+//                                    @Override
+//                                    public void onFailure(@NonNull Exception e) {
+//                                        Log.e("Firestore", "Error adding event with checkbox selections", e);
+//                                    }
+//                                });
+//                    }
+//                }
+//            }
+//        });
     }
 }
