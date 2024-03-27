6d92e1c804e2adacbefa3073b823b69fd76117f0
Akhil Pullela <akpullela2@gmail.com>
2024-03-27 11:46:29 -0600

    Got map to show a specific geopoint

--- app/src/main/java/com/example/project_3/EventMapFragment.java
+++ app/src/main/java/com/example/project_3/EventMapFragment.java
@@ -1,31 +1,64 @@
 package com.example.project_3;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.views.MapView;
+
+import java.util.ArrayList;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.GeoPoint;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.views.MapView;
-import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.overlay.Marker;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
-
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+//source: https://github.com/osmdroid/osmdroid/wiki/How-to-use-the-osmdroid-library-(Java)
 public class EventMapFragment extends Fragment  {
-    private MapView map = null;
+    private MapView map;
+    private FirebaseFirestore db;
+    private CollectionReference profilesRef;
+    private GeoPoint location;
+    private final int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.map_fragment, container, false);
-
+        Context ctx = getContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
         map = view.findViewById(R.id.map);
         TextView appbar = view.findViewById(R.id.appbar_title);
         Button back = view.findViewById(R.id.back_button);
@@ -36,14 +69,18 @@ public class EventMapFragment extends Fragment  {
                 getActivity().findViewById(R.id.rest_attendees_list).setVisibility(View.VISIBLE);
             }
         });
-        appbar.setText("Event Map");
-        map.setMultiTouchControls(true);
-        GeoPoint startPoint = new GeoPoint(48.8583, 2.2944);  // This is the GeoPoint for Eiffel Tower, Paris
-        map.getController().setCenter(startPoint);
+        map = (MapView) view.findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
+        String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.ACCESS_FINE_LOCATION};
+        requestPermissionsIfNecessary(permissions);
+        IMapController mapController = map.getController();
+        org.osmdroid.util.GeoPoint start = new org.osmdroid.util.GeoPoint(47.5952, -122.3316);
+        mapController.setCenter(start);
+        mapController.setZoom(17);
 
         return view;
     }
-    
+
     @Override
     public void onResume() {
         super.onResume();
@@ -53,7 +90,37 @@ public class EventMapFragment extends Fragment  {
     @Override
     public void onPause() {
         super.onPause();
-        map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
+        map.onPause();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        ArrayList<String> permissionsToRequest = new ArrayList<>();
+        for (int i = 0; i < grantResults.length; i++) {
+            permissionsToRequest.add(permissions[i]);
+        }
+        if (permissionsToRequest.size() > 0) {
+            ActivityCompat.requestPermissions(
+                    this.getActivity(),
+                    permissionsToRequest.toArray(new String[0]),
+                    REQUEST_PERMISSIONS_REQUEST_CODE);
+        }
+    }
+
+    private void requestPermissionsIfNecessary(String[] permissions) {
+        ArrayList<String> permissionsToRequest = new ArrayList<>();
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this.getContext(), permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                // Permission is not granted
+                permissionsToRequest.add(permission);
+            }
+        }
+        if (permissionsToRequest.size() > 0) {
+            ActivityCompat.requestPermissions(
+                    this.getActivity(),
+                    permissionsToRequest.toArray(new String[0]),
+                    REQUEST_PERMISSIONS_REQUEST_CODE);
+        }
     }
 }
-
