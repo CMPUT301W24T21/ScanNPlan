0373194108f7238341835087f6b7af9417afb183
AidenKly <aklymchu@ualberta.ca>
2024-04-06 13:40:08 -0600

    removed had coded profileID from attendeeActivity, restored profile creation functionality for mainactivity, fixed AttendeeNewEventDetailsFragment return to main page, fixed geologation toggles and MaterialSwitch classes in AttendeeEditProfileFragment and profile_settings.xml

--- app/src/main/java/com/example/project_3/AttendeeEditProfileFragment.java
+++ app/src/main/java/com/example/project_3/AttendeeEditProfileFragment.java
@@ -30,6 +30,7 @@ import androidx.fragment.app.FragmentTransaction;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.materialswitch.MaterialSwitch;
 import com.google.android.material.switchmaterial.SwitchMaterial;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldValue;
@@ -90,7 +91,7 @@ public class AttendeeEditProfileFragment extends Fragment {
                 }, 500); // Adjust the delay time as needed
             }
         });
-        SwitchMaterial geolocation = view.findViewById(R.id.geolocation_toggle);
+        MaterialSwitch geolocation = view.findViewById(R.id.geolocation_toggle);
         geolocation.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -151,8 +152,12 @@ public class AttendeeEditProfileFragment extends Fragment {
                     String socialLink = documentSnapshot.getString("social_link");
                     String base64Image = documentSnapshot.getString("profile_image");
                     Boolean location_status = documentSnapshot.getBoolean("locationEnabled");
+                    if (location_status == null){
+                        location_status = false;
+                        db.collection("Profiles").document(profileID).update("locationEnabled", Boolean.FALSE);
+                    }
                     nameTextView.setText(profileName);
-                    geolocation.setChecked(location_status);
+                    geolocation.setChecked(Boolean.TRUE.equals(location_status));
                     contactInfoTextView.setText(contactInfo);
                     socialLinkTextView.setText(socialLink);
 
